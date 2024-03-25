7f24e0f2c0bc486dc6598000499558be5a9f0c61
TheDolphinCoder11 <153998936+TheDolphinCoder11@users.noreply.github.com>
2024-03-25 13:58:47 +0800

    Create index.js

---
+++ index.js
@@ -0,0 +1,30 @@
+function horoscope() {
+    let birthMonth = "October"
+    if (birthMonth == "January") {
+        console.log("A new opportunity will arise soon.")
+    } else if (birthMonth == "Febuary") {
+        console.log("Financial prosperity is on the horizon.")
+    } else if (birthMonth == "March") {
+        console.log("Unexpected travel may be in your future.")
+    } else if (birthMonth == "April") {
+        console.log("Your hard work will pay off handsomely.")
+    } else if (birthMonth == "May") {
+        console.log("A period of growth and transformation awaits you.")
+    } else if (birthMonth == "June") {
+        console.log("Luck will be on your side in the coming weeks.")
+    } else if (birthMonth == "July") {
+        console.log("Family bonds will strengthen in the near future.")
+    } else if (birthMonth == "August") {
+        console.log("Your dedication will lead to significant achievements.")
+    } else if (birthMonth == "September") {
+        console.log("A positive change is on the horizon.")
+    } else if (birthMonth == "October") {
+        console.log("Your charisma will attract success and happiness.")
+    } else if (birthMonth == "November") {
+        console.log("Unexpected blessings will come your way")
+    } else if (birthMonth == "December") {
+        console.log("Joy and abundance will fill your life this month")
+    }
+}
+
+horoscope();
